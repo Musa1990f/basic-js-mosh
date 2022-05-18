1cbdd9c5213d4026df926a9afb083937fe4f9fd4
Lukenge Faridu Musa <95524786+Musa1990f@users.noreply.github.com>
2022-05-19 04:09:44 +0900

    added basic js about object literals by mosh-tutorials

---
+++ index.js
@@ -0,0 +1,14 @@
+// Object operators
+const person = {
+  name: 'musa',
+  age: 30
+};
+
+// we can use dot notation!
+person.name = 'mus';
+// dot notation is best used 
+
+// we acn also use [] notation
+//let selection = 'name';
+//person[selection] = 'john'
+console.log(person.name)
